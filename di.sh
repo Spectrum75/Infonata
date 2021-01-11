012836dd5197e54ef829616d89c07e4576fda67e
Spectrumgamer75 <72115696+spectrumgamer75@users.noreply.github.com>
2021-01-11 11:36:49 +0530

    Added Script For Distro Info

---
+++ di.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Distro information script (part of Infonata)
+echo -e "\e[31mDistro And Software Information:"
+# THESE SPACES ARE IMPORTANT FOR CORRECT DISTANCE BETWEEN THE OUTPUTS.
+echo -en '\n'
+# SPACE
+echo -en '\n'
+# SPACE
+neofetch
