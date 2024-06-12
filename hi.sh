fc43a3b63cffa4c174a7fbd7b650770689e72ea7
Spectrum75 <72115696+Spectrum75@users.noreply.github.com>
2024-06-12 21:00:45 +0530

    Delete hi.sh
    Deprecate hi.sh

--- hi.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Hardware Info (Part Of Infonata Script)
-echo -en '\n'
-echo -e "\e[7mHardware Information:\e[0m"
-echo -en '\n'
-inxi -Fxxx
-echo -en '\n'
-echo -en '\n'
-# End
